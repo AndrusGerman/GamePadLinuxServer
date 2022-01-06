4e403584b6b6e2c040e336a2ca5c205dae10fdb7
AndrusGerman <andrusdiazaleman@gmail.com>
2022-01-06 03:46:52 -0400

    key fixed

--- ws.go
+++ ws.go
@@ -171,22 +171,27 @@ func init() {
 func (ctx *Events) ManagerJoystickKeyboard(keyboard uinput.Keyboard) {
 	switch ctx.Mode {
 	case 1:
-		keys := strings.Split(ctx.Value, ",")
-		for _, v := range keys {
-			// Press Buttom
-			key := strings.Split(v, "-")
+		keyType := strings.Split(ctx.Value, "|")
+		keysAdd := strings.Split(keyType[0], ",")
+		keyRemove := strings.Split(keyType[1], ",")
+		for _, key := range keyRemove {
+
+			// Suelta las demas
+			vl, ok := keyMap[key]
+			if ok {
+				keyboard.KeyUp(int(vl))
+			}
+
+		}
+
+		for _, key := range keysAdd {
 
 			// Preciona la selecionada
-			vl, ok := keyMap[key[0]]
+			vl, ok := keyMap[key]
 			if ok {
 				keyboard.KeyDown(int(vl))
 			}
 
-			// Suelta la contraria
-			vl, ok = keyMap[key[1]]
-			if ok {
-				keyboard.KeyUp(int(vl))
-			}
 		}
 	case 3:
 		keys := strings.Split(ctx.Value, ",")
