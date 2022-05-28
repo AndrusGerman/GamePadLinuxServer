09504f1c6da47ae63776aff669d70a4d92296edd
AndrusGerman <andrusdiazaleman@gmail.com>
2022-05-28 04:16:21 -0400

    an example of use is added to measure latency

--- pkg/events/event_latency.go
+++ pkg/events/event_latency.go
@@ -8,6 +8,12 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// To measure the latency you send the last value of time now, which the server answered you
+// and in this way the first value you will get is the current latency and the new value of time now
+// you send -> Last time.Now() value ($2)  | from the last reply     $1|$2
+// --
+// you get <- $1|$2 | example 20|232323232323
+// the first value is the latency, and the second is the last time.Now()
 func (ctx *Events) ManagerLatency(ws *websocket.Conn) {
 	switch ctx.Mode {
 	case 1:
@@ -20,3 +26,10 @@ func (ctx *Events) ManagerLatency(ws *websocket.Conn) {
 		ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("%d|%d", tt, time.Now().UnixNano())))
 	}
 }
+
+// -> 0
+// <- Any|200000
+// -> 200000
+// <- 3|201003 // your latency is 3
+// -> 201003
+// <- 20|202023 // your latency is 20...
